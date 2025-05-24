5a6223377218c4d926dcafc2369dc251492b2692
Jonas Ferrao <jonaschris2103@gmail.com>
2025-05-24 12:13:23 +0530

    Test with Azure Deployed Container

--- app/test.py
+++ app/test.py
@@ -3,13 +3,14 @@ import asyncio
 
 
 async def send_audio():
+    # url = "https://mvaakasr-fjfsfhftevbdayhf.westindia-01.azurewebsites.net/transcribe"
     url = "http://localhost:8000/transcribe"
     file_path = "audio/sample.wav"
 
     async with httpx.AsyncClient() as client:
         with open(file_path, "rb") as f:
             files = {"file": ("sample.wav", f, "audio/wav")}
-            response = await client.post(url, files=files, timeout=60)
+            response = await client.post(url, files=files, timeout=90)
 
         print("Status code:", response.status_code)
         print("Response:", response.text)
